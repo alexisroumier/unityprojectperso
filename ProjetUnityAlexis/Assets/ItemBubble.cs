89f015e4991055f0a532f443358fb0c2c3ddfcce
alexplay78 <100867591+alexplay78@users.noreply.github.com>
2022-06-18 16:08:25 +0200

    minifix sprite bubble

--- ProjetUnityAlexis/Assets/ItemBubble.cs
+++ ProjetUnityAlexis/Assets/ItemBubble.cs
@@ -34,18 +34,21 @@ public class ItemBubble : MonoBehaviour
     void Update()
     {
         transform.rotation = Quaternion.Euler(x,y,z);
-        spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
+        
+        if(spriteBubble)spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
     }
 
     void LateUpdate()
     {
-                spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
+        if(spriteBubble)spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
     }
 
     public void SpriteBubble()
     {
-        spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
         bubbleItem = EventSystem.current.currentSelectedGameObject.GetComponent<InventorySlot>().item;
+        spriteBubble = CluesInventory.instance.objectsToActivate[2];
+        Bubble = CluesInventory.instance.objectsToActivate[1];
+        spriteBubble.GetComponent<SpriteRenderer>().size = new Vector2(1,1);
         if(bubbleItem != null)
         {
             if(!Bubble.activeSelf)
@@ -53,24 +56,27 @@ public class ItemBubble : MonoBehaviour
                 bubbleItem.IsSelected = true;
                 Debug.Log(bubbleItem.name + " = " + bubbleItem.IsSelected);
                 Bubble.SetActive(true);
+                Debug.Log(bubbleItem.image);
                 spriteBubble.GetComponent<SpriteRenderer>().sprite = bubbleItem.image;
             }
             else
             {
-                if(Bubble.activeSelf && bubbleItem.IsSelected == true)
+                Debug.Log("in else");
+                if(Bubble.activeSelf)
                 {
                     bubbleItem.IsSelected = false;
                     Debug.Log(bubbleItem.name + " = " + bubbleItem.IsSelected);
                     Bubble.SetActive(value: false);
                     spriteBubble.GetComponent<SpriteRenderer>().sprite = null;
                 }
-
+/*
                 if(Bubble.activeSelf && bubbleItem.IsSelected == false)
                 {
                     spriteBubble.GetComponent<SpriteRenderer>().sprite = bubbleItem.image;
                     bubbleItem.IsSelected = true;
                     Debug.Log(bubbleItem.name + " = " + bubbleItem.IsSelected);
                 }
+*/
             }
         }
         else
